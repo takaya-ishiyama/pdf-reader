ed0dd9e6313b7704095b06c7314aedefbcbe9195
takaya-ishiyama <takaya.ishiyama00810@gmail.com>
2026-06-23 22:04:25 +0900

    fix

--- apps/api/src/domain/document/document.rs
+++ apps/api/src/domain/document/document.rs
@@ -1,12 +1,37 @@
 use thiserror::Error;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct DocumentId(pub(crate) uuid::Uuid);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Document {
-    pub id: String,
-    pub title: String,
+    pub(crate) id: DocumentId,
+    pub(crate) title: String,
+    pub(crate) latest_read_line: i32,
+    pub(crate) version: i16,
+    pub(crate) document_url: String,
 }
 
 #[derive(Debug, Error)]
 pub enum DocumentError {
     #[error("invalid id: {0}")]
     InvalidID(String),
+}
+
+impl Document {
+    pub(crate) fn new(
+        id: DocumentId,
+        title: String,
+        latest_read_line: i32,
+        version: i16,
+        document_url: String,
+    ) -> Self {
+        Self {
+            id,
+            title,
+            latest_read_line,
+            version,
+            document_url,
+        }
+    }
 }
